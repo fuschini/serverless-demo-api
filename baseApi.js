b7c2c7413b44ab76f341fa8c0372ab23f841ff70
Henrique Fuschini Favaro <henriquefuschini@gmail.com>
2022-10-25 20:12:58 -0500

    initial commit

---
+++ baseApi.js
@@ -0,0 +1,16 @@
+module.exports.handler = async (event) => {
+  
+  console.log(`Request IP address: ${event?.requestContext?.http?.sourceIp}\nRequest timestamp: ${event?.requestContext?.time}`);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Hello Lunch and Learn participant!",
+        input: event
+      },
+      null,
+      2
+    ),
+  };
+};
---
+++ fusca.js
@@ -0,0 +1,11 @@
+module.exports.handler = async (event) => {
+  console.log(`Request IP address: ${event?.requestContext?.http?.sourceIp}`);
+
+  const imageURL = 'https://picsum.photos/200/300'
+
+  return {
+    statusCode: 200,
+    headers: { "content-type": "text/html" },
+    body: `<html><img src="${imageURL}"></html>`
+  };
+};
